d4e3ec9a5991d9fcde38a64d1b2f2d6c4488764f
leejiwoo <leejiwoo2002@gmail.com>
2023-01-08 22:53:52 +0900

    feat : BlockQuote 명세 추가

--- src/components/quote/quote.tsx
+++ src/components/quote/quote.tsx
@@ -16,9 +16,12 @@ const Quote = ({children}: StrongProps) => {
 export default Quote;
 
 const Container = styled('div')(() => ({
-  borderLeft: '3px solid',
+  borderLeft: '4px solid',
   borderColor: colors.green[400],
-  paddingLeft: '10px'
+  backgroundColor: 'rgb(0, 150, 0, 0.1)',
+  paddingLeft: '20px',
+  paddingTop: '1px',
+  paddingBottom: '1px'
 }));
 
 
